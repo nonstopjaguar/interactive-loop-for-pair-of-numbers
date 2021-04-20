73110ec51d66b53aa487477c951351f208490ca6
lance <68605558+nonstopjaguar@users.noreply.github.com>
2021-04-20 13:50:09 +0800

    Update interactive loop.c

--- interactive loop.c
+++ interactive loop.c
@@ -6,11 +6,11 @@ int main()
 {
     int l1,l2; //used to declare variables to store two numbers
     
-    printf("Input pair of numbers: ");
+    printf("Input pair of integers: ");
     scanf("%d%d",&l1,&l2);      //input two integers
     
     while(! (l2/l1==2 && l2%l1==0)){ //continue in loop untill l1 exactly divides l2 to two halves
-        printf("Input pair of numbers: ");
+        printf("Input pair of integers: ");
         scanf("%d%d",&l1,&l2);  //input another pair of integers
     }
     return 0;
